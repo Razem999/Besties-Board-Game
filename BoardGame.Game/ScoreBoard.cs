e25e671cdafe61e23a9c5be50cccde26f8499275
Razem Muhammad Shahin <razem999@gmail.com>
2026-09-06 22:22:02 -0400

    Update ScoreBoard.cs
    Added properties and methods to initialize scoreboard with players and add points to players.

--- BoardGame.Game/ScoreBoard.cs
+++ BoardGame.Game/ScoreBoard.cs
@@ -6,5 +6,31 @@ namespace BoardGame.Game
 {
     internal class ScoreBoard
     {
+        private readonly Dictionary<string, int> _scores = new();
+        public IReadOnlyDictionary<string, int> Scores => _scores;
+
+        public void InitializePlayerScores(List<Player> players)
+        {
+            foreach (var player in players)
+            {
+                _scores[player.playerId] = 0;
+            }
+        }
+
+        public void AddRoundScores(Round round)
+        {
+            foreach (var playerScore in round.playerRoundScores)
+            {
+                if (_scores.ContainsKey(playerScore.playerId))
+                {
+                    _scores[playerScore.playerId] += playerScore.roundScore;
+
+                }
+                else
+                {
+                    _scores[playerScore.playerId] = playerScore.roundScore;
+                }
+            }
+        }
     }
 }
