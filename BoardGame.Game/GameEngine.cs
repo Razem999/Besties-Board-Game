7775bc1edb2b287b972c0e3fe38f41bae0639860
Razem Muhammad Shahin <razem999@gmail.com>
2026-09-09 13:58:37 -0400

    Initial GameEngine Update
    Added methods to be invoked by the GameEngine object.

--- BoardGame.Game/GameEngine.cs
+++ BoardGame.Game/GameEngine.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace BoardGame.Game
 {
+    enum GameStatus
+    {
+        INPROGRESS,
+        COMPLETE,
+        INLOBBY
+    }
     internal class GameEngine
     {
+        public GameState StartGame(GameState state)
+        {
+            return state;
+        }
+
+        public GameState EndGame(GameState state)
+        {
+            return state;
+        }
+
+        public GameState NextTurn(GameState state)
+        {
+            return state;
+        }
+
+        public GameState EndTurn(GameState state)
+        {
+            return state;
+        }
+
+        public GameState NextRound(GameState state)
+        {
+            return state;
+        }
     }
 }
