30b9077e0b24701a232abba358194ec353b483f1
Razem Muhammad Shahin <razem999@gmail.com>
2026-09-08 22:20:51 -0400

    Update GameState.cs
    Added initial fields and methods for the GameState object.

--- BoardGame.Game/GameState.cs
+++ BoardGame.Game/GameState.cs
@@ -6,5 +6,42 @@ namespace BoardGame.Game
 {
     internal class GameState
     {
+        public string gameId { get; set; }
+        public GameStatus status = GameStatus.INLOBBY;
+        public List<Player> playerList { get; set; } = new();
+        public Player currentStar { get; set; }
+        public int totalRounds = 0;
+        public List<Round> rounds { get; set; } = new();
+        public Round currentRound { get; set; }
+        public Deck gameDeck { get; set; }
+        public ScoreBoard scoreBoard { get; set; }
+
+        public GameState(string gameId, int totalRounds)
+        {
+            this.gameId = gameId;
+            this.status = GameStatus.INLOBBY;
+            this.totalRounds = totalRounds;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            this.playerList.Add(player);
+        }
+
+        public void RemovePlayer(string playerId)
+        {
+            int removedCount = playerList.RemoveAll(player => player.playerId == playerId);
+
+            if (removedCount > 0)
+            {
+                // Clean up network connections
+                // Broadcast to other players
+                Console.WriteLine("Successfully removed player");
+            } 
+            else
+            {
+                Console.WriteLine("Failed to remove player");
+            }
+        }
     }
 }
